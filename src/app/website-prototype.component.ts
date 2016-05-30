2f359798dea1b89bed7874e5c9ca845a271fb550
Alex Olivas <alex.olivas11@gmail.com>
2016-05-30 15:53:22 -0400

    Initial signup route code

--- src/app/website-prototype.component.ts
+++ src/app/website-prototype.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { SignupComponent } from './+signup';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
-  moduleId: module.id,
-  selector: 'website-prototype-app',
-  templateUrl: 'website-prototype.component.html',
-  styleUrls: ['website-prototype.component.css']
+    moduleId: module.id,
+    selector: 'website-prototype-app',
+    templateUrl: 'website-prototype.component.html',
+    styleUrls: ['website-prototype.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+    {path: '/signup', component: SignupComponent}
+])
 export class WebsitePrototypeAppComponent {
-  title = 'website-prototype works!';
+    title = 'website-prototype works!';
 }
